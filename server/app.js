416efdcf9020cf88f674c009d3f13f5c6a4217d5
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-08-21 23:58:51 -0500

    adding text on homepage

--- server/app.js
+++ server/app.js
@@ -12,18 +12,23 @@ app.use(bodyParser.json());
 app.use( express.static('public'));
 
 
+// Route Inclusion
+
+var base = require('./routes/coffee');
+
+app.use('/', base);
 
 //base url
-router.get( '/', function ( req, res ){
-  console.log( 'at base url' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
-});
+// router.get( '/', function ( req, res ){
+//   console.log( 'at base url' );
+//   // res.sendFile( path.resolve( 'views/index.html' ) );
+// });
 
-router.get('/sunshine', function(req, res){
-  console.log('hello');
-  res.sendFile( path.resolve( 'public/views/index.html' ) );
-
-});
+// router.get('/sunshine', function(req, res){
+//   console.log('hello');
+//   res.sendFile( path.resolve( 'public/views/index.html' ) );
+//
+// });
 
 // Spinning up the server
 app.listen(process.env.PORT || 3000, function(){
--- server/routes/coffee.js
+++ server/routes/coffee.js
@@ -6,4 +6,9 @@ var router = express.Router();
 var pg = require('pg');
 
 
+router.get('/', function ( req,res ){
+  res.sendFile(path.resolve('public/views/index.html'));
+
+});
+
 module.exports = router;
